a406617b1198d3caf9d7fe904b4a51a3856a4e50
AtuZ7 <artur.kytt7@gmail.com>
2026-02-27 21:15:12 +0200

    feat: bootstrap spring boot project

---
+++ src/test/java/com/arturkytt/restaurantreservationsystem/RestaurantReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturkytt.restaurantreservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReservationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
